a1bc7f5a40d5dac5dff7f74421366b33f878b297
Joyce Chun <wooyangee@gmail.com>
2018-04-17 05:00:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase=)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
