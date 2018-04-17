d51e2884a68ba6e1022a100835e69c24e81e88f7
Joyce Chun <wooyangee@gmail.com>
2018-04-17 05:10:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ def speak_to_grandma(phrase="Grandma, do you bake?")
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
